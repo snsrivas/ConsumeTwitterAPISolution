b6e809f7499543802f524974f6483e6be647a76f
Saurabh Srivastava <52217503+snsrivas@users.noreply.github.com>
2022-12-18 21:44:17 -0600

    Add project files.

---
+++ ConsumeTwitterAPI/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using ConsumeTwitterAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TwitterApiBorker;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsumeTwitterAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        public string ApiMessage { get; set; }
+        public string TotalCount { get; set; }
+        public string AverageTweetValue { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration iConfiguration)
+        {
+            _logger = logger;
+            _configuration = iConfiguration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+       
+        
+        public IActionResult GetTotalCount()
+        {
+            TwitterBroker twitterApiBroker = new TwitterBroker();
+            var twitterDomain = _configuration["ApiDomainName"];
+            var bearerToken = _configuration["APiBearerToken"];
+            var totalTweets = twitterApiBroker.GetTotalTweets(twitterDomain, bearerToken);
+            if (totalTweets != null)
+            {
+                ViewBag.TotalCount = totalTweets.Result;
+                return View("Index");
+            }
+            else
+            {
+                ApiMessage = "Not Successful";
+                return NotFound();
+            }
+        }
+      
+       
+        public IActionResult AverageNoOfTweets()
+        {
+            TwitterBroker twitterApiBroker = new TwitterBroker();
+            int averageNoOfTweets = twitterApiBroker.AverageNoOfTweets(_configuration["ApiDomainName"], _configuration["APiBearerToken"]);
+            if (averageNoOfTweets != 0)
+            {
+                ViewBag.AverageTweetValue = averageNoOfTweets;
+                return View("Index");
+            }
+            else
+            {
+                ApiMessage = "Not Successful";
+                return NotFound();
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TwitterApiBroker/TwitterBroker.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TwitterApiBorker
+{
+    public class TwitterBroker
+    {
+        public string FromDate { get; set; }
+
+        public string EndDate { get; set; }
+
+        public async Task<string> GetTotalTweets(string twitterDomain, string bearerToken)
+        {
+            try
+            {
+                this.FromDate = this.GetUTCDate(7);
+                this.EndDate = this.GetUTCDate(0);
+
+                string getURL = string.Format("{0}/2/tweets/counts/recent?query={1}&start_time={2}&end_time={3}", twitterDomain, 10, this.FromDate, this.EndDate);
+
+                var requestUserTimeline = new HttpRequestMessage(HttpMethod.Get, getURL);
+                requestUserTimeline.Headers.Add("Authorization", "Bearer " + bearerToken);
+
+                using (var httpClient = new HttpClient())
+                {
+                    await Task.Delay(12000);
+                    using (HttpResponseMessage response = await httpClient.SendAsync(requestUserTimeline))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var json = JsonSerializer.Deserialize<object>(await response.Content.ReadAsStringAsync());
+                            JToken jToken = JToken.Parse(json.ToString());
+                            if (json != null)
+                            {
+                                return ((JValue)jToken.SelectToken("meta").SelectToken("total_tweet_count")).Value.ToString();
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex) {   throw ex;   }
+        }
+
+        public string GetUTCDate(int subtract)
+        {
+            DateTime theDate = DateTime.Now.ToUniversalTime();
+            if (subtract != 0)
+                theDate -= new TimeSpan(subtract - 1, 20, 55, 0);
+            return (theDate.ToString("s") + "Z");
+        }
+
+        //public string GetTotalNumberOfTweets(string twitterDomain, string bearerToken)
+        //{
+        //    var totalTweets = this.GetTotalTweets(twitterDomain, bearerToken);
+
+        //    if (totalTweets != null)
+        //    {
+        //        return totalTweets;
+        //    }
+            
+        //    return 0;
+        //}
+
+        public int AverageNoOfTweets(string twitterDomain, string bearerToken)
+        {
+            var totaNoOfTweets = this.GetTotalTweets(twitterDomain, bearerToken);
+            if (totaNoOfTweets != null)
+            {
+                int totalTweets = Convert.ToInt32(totaNoOfTweets.Result.ToString());
+                var diffMinutes = (DateTime.Parse(this.EndDate.Substring(0, this.EndDate.Length - 1)) - DateTime.Parse(this.FromDate.Substring(0, this.FromDate.Length - 1))).TotalMinutes;
+
+                return (Convert.ToInt32(totalTweets) / (int)diffMinutes);
+            }
+            return 0;
+        }
+    }
+}
